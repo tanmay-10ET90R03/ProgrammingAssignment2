3da30be71b91390fb8849ccd0b8a9b761c18298f
tanmay-10ET90R03 <tanmay-10ET90R03@users.noreply.github.com>
2020-06-05 02:43:09 +0530

    Add files via upload

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,10 +1,11 @@
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  inv <- x$getInverse()
-  if(!is.null(inv)){
-    message("getting cached data")
+  inv <- x$getInverse() 
+  if(!is.null(inv)){ 
+    message("getting cached data") 
     return(inv)
   }
+  ##If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache
   data <- x$get()
   inv <- solve(data)
   x$setInverse(inv)
--- makeCachematrix.R
+++ makeCachematrix.R
@@ -1,11 +1,11 @@
-makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
+makeCacheMatrix <- function(x = matrix()) {  ## function declaration
+  inv <- NULL ##initially no inverse is calculated
   set <- function(y){
     x <<- y
     inv <<- NULL
   }
   get <- function() x
-  setInverse <- function(solveMatrix) inv <<- solveMatrix
+  setInverse <- function(solveMatrix) inv <<- solveMatrix  ##Set inverse by solvematrix
   getInverse <- function() inv
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
